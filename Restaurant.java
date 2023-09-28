b4e8d31e09e2f9b84d87b77c8280376ee1d33c58
netabirami <net.abirami@gmail.com>
2023-09-28 23:44:43 +0200

    created a new table class

---
+++ Restaurant.java
@@ -0,0 +1,14 @@
+public class Restaurant {
+    private String foodMenu;
+    private  int tables;
+    private String orders;
+    public void  addFoodItem(){
+
+    }
+    public void addTable(){
+
+    }
+    public void placeOrder(){
+
+    }
+}
