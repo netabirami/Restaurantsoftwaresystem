4121cc90ffb4ff624530bc70fda990f29965e18b
netabirami <net.abirami@gmail.com>
2023-09-28 23:36:57 +0200

    created a table class

---
+++ Order.java
@@ -0,0 +1,12 @@
+public class Order {
+    private int orderID ;
+    private String foodItems;
+    private double totalPrice;
+    public void addFoodItem(){
+
+    }
+    public void calculateTotal(){
+
+    }
+
+}
